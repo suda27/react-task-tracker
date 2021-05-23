e3ca92879088e4876263c5169a553775d3ad14e6
Sudarsan Prabakaran <sudarsan.prabakaran@coda.global>
2021-05-23 15:29:17 +0530

    Added conole log Authorization token call

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import AddTask from "./components/AddTask";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./components/About";
+// import UserPool from './UserPool';
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false);
 
   const [tasks, setTasks] = useState([]);
-
+ 
   useEffect(() => {
     const getTasks = async () => {
+      const dataTemp = await authorizerRequest();
+      console.log(dataTemp)
       const taskFromServer = await fetchTasks();
       setTasks(taskFromServer);
     };
     getTasks();
+
   }, []);
 
   //Fetch Tasks
@@ -26,6 +30,18 @@ const App = () => {
     return data;
   };
 
+  //Authorizer Request
+  const authorizerRequest = async () => {
+    const token = 'eyJraWQiOiJtaHJZUW9mR2Y5UzRGVXJOVzNjcktlbmNaMFwvYjNDMUVTeUNMZVl1SlYrUT0iLCJhbGciOiJSUzI1NiJ9.eyJhdF9oYXNoIjoiWVV5N25CSm1RbkFBYWc4dGdHSWpIdyIsInN1YiI6IjRhMjUyZWVmLTg4MmYtNGMyMC1iNGEwLWQ3NzFlYWJkNDhmYyIsImNvZ25pdG86Z3JvdXBzIjpbInVzLWVhc3QtMV9JSThleURYcG9fR29vZ2xlIl0sImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX0lJOGV5RFhwbyIsImNvZ25pdG86dXNlcm5hbWUiOiJnb29nbGVfMTEwMTMzOTcxNDk3MTI4ODY1NjkxIiwibm9uY2UiOiJ1TFFHd1BHcWNYTlV1VHdBd0NVdUhPejV5bGJLN3pjRkppYWxpRDBUalZ0OHhsZkhLSTJjV0YzS0wyZU5xZWpHRVdUd213ZnFQT2ZVdzFJaGRxOUVweFlMQXc4X2FKZGxRel9zUXN2bXo5NFVRN1F6dEZJWWpJQWFaMmc5NG04TEZMMVNYQ3pCLTNCaUNEMXZPSDdkV1ZIZFl4aTBIUWRhSTFNUmNwZzFpSTQiLCJwaWN0dXJlIjoiaHR0cHM6XC9cL2xoMy5nb29nbGV1c2VyY29udGVudC5jb21cL2EtXC9BT2gxNEdnTlBGM2wxTzdVd283dEJZUzZoNy1TcUM1Y1hodklNNHlPX2lrYT1zOTYtYyIsImF1ZCI6IjNqZWduY2M0c242aDJ2aW1jbzUwYjZwaW8xIiwiaWRlbnRpdGllcyI6W3sidXNlcklkIjoiMTEwMTMzOTcxNDk3MTI4ODY1NjkxIiwicHJvdmlkZXJOYW1lIjoiR29vZ2xlIiwicHJvdmlkZXJUeXBlIjoiR29vZ2xlIiwiaXNzdWVyIjpudWxsLCJwcmltYXJ5IjoidHJ1ZSIsImRhdGVDcmVhdGVkIjoiMTYyMTc1MjkxODc1OCJ9XSwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE2MjE3NjAwMTIsIm5hbWUiOiJTdWRhcnNhbiBTYW4iLCJleHAiOjE2MjE3NjM2MTIsImlhdCI6MTYyMTc2MDAxMiwiZW1haWwiOiJzdXNhc2FuMjdAZ21haWwuY29tIn0.fkbHDIUxUnPQ750x2QpZKYbK8kdAbBl2sW_RJnRrZIqfA_TniqLFnZj2WxZ-hVcXt464OhUjclFMbOxAGf2fSkOfXZAfQBfNjB8RAoVEk9ba5mR3BpxnVNVvzkZfWSRu4BKNnRCjMKnPhlKQ3aR-gDH-ATJjZt8a9SftIqJqaklsyYrBAE0hbxnCYl_aVVhenM9sb7HVttv9g702o5TFYeupNwofXbIW-yl8cZnlgr5Duy-J3S-FpiYEM8iFQFR5Z1pxSH9EGQUuooTGRC68Yr8JMwKRAsqmDgljEOKrjfxf09GdfRpube7fdR_M5rSIRu2CuHUAP2vW4yK2JDHU2Q';
+    const res = await fetch("https://qh8ff4qe0k.execute-api.us-east-1.amazonaws.com/dev/get-user/123",{
+      method: 'GET',
+      headers: {
+        'Authorization':token
+      }
+    });
+    const data = await res.json();
+    return data;
+  };
   //Fetch Individual Task
   const fetchTask = async id => {
     const res = await fetch(`http://localhost:5000/tasks/${id}`);
@@ -86,6 +102,8 @@ const App = () => {
       )
     );
   };
+  
+
 
   return (
     <Router>
